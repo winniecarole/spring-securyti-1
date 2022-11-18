aafefbac5abe3160db203e98ecfaa402d1b9913a
tonglw <Winnie.Tongle.Djoukeng@msg.group>
2022-11-18 17:45:26 +0100

    initial commit

---
+++ src/test/java/com/example/sringjwt/SringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sringjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
